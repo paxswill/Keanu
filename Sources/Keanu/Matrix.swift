eb21c9f6137c695df8f78b27ac3d43d8c9ef32ea
Will Ross <paxswill@paxswill.com>
2020-04-18 19:30:49 -0400

    Clean up Matrix and MatrixView for swift-format

--- Sources/Keanu/Matrix.swift
+++ Sources/Keanu/Matrix.swift
@@ -1,21 +1,45 @@
 // I would like to put this within the Matrix class, but it is not a
 // generic type, so it's easier to use it when it's outside.
+/// An enum for representing if a matrix is using a row-major or column-major
+/// layout.
+///
+/// This difference exposes itself in a few places, mainly when iterating over
+/// elements in the matrix or when using vector operations on the elements of
+/// the matrix.
 public enum Order {
+    /// Row-major ordering.
     case rowMajor
+
+    /// Column-major ordering.
     case columnMajor
 }
 
+/// Type representing a two-dimensional matrix.
+///
+/// All indexes start from 0. The dimensions of the matrix are immutable, but
+/// each element's value *is* mutable.
 public class Matrix<T>: ExpressibleByArrayLiteral {
+    /// The type of elements contained within this matrix.
     public typealias Element = T
+
+    /// The type of the indicies used to access elements in this matrix.
     public typealias Index = Int
 
+    /// The number of rows in the matrix.
     public let rowCount: Index
+
+    /// The number of columns in the matrix.
     public let columnCount: Index
+
+    /// Whether this matrix is using a row-major or column-major representation
+    /// internally.
     public private(set) var order: Order
+
     @usableFromInline internal var storage: ContiguousArray<Element> = []
 
     // MARK: Initializers
 
+    /// Initialize a matrix with a single repeated value.
     public init(rows: Index, columns: Index, initialValue: Element, order: Order = .rowMajor) {
         rowCount = rows
         columnCount = columns
@@ -24,6 +48,15 @@ public class Matrix<T>: ExpressibleByArrayLiteral {
     }
 
     // TODO: genericize the elementArrays argument
+    /// Initialize a matrix from a two-dimensional array of Elements.
+    ///
+    /// - Parameters:
+    ///     - elementArrays: A 2D-array of Elements. The inner arrays *must* all
+    ///       have the same number of elements.
+    ///     - sourceOrder: Declare whether `elementArrays` is a row-major or
+    ///       column-major representation of a matrix.
+    ///     - order: What the newly created matrix should use for storing the
+    ///       elements.
     public init(
         _ elementArrays: [[Element]], sourceOrder: Order = .rowMajor, order: Order = .rowMajor
     ) {
@@ -59,26 +92,39 @@ public class Matrix<T>: ExpressibleByArrayLiteral {
         }
     }
 
+    /// Create a new matrix with an array literal.
+    ///
+    /// The given array literal must be a row-major representation, and the
+    /// resulting Matrix will also be stored in a row-major format. If you need
+    /// control over those defaults, the `init(_:, sourceOrder:, order:)`
+    /// initializer is a better choice.
+    ///
+    /// - Parameter arrayLiteral: a variadic argument of arrays of Elements.
     required public convenience init(arrayLiteral: [Element]...) {
         self.init(arrayLiteral)
     }
 
     // MARK: Aggregate access
 
+    /// The total number of elements in the matrix.
     public var count: Int {
         return storage.count
     }
 
     // MARK: Element Access
 
+    /// Returns a `RowView` for the given row.
     public func row(_ rowIndex: Index) -> RowView<Element> {
         return RowView(self, index: rowIndex)
     }
 
+    /// Returns a `ColumnView` for the given column.
     public func column(_ columnIndex: Index) -> ColumnView<Element> {
         return ColumnView(self, index: columnIndex)
     }
 
+    /// Internal helper the calculates the proper index in the backing store for
+    /// the given row and column.
     @inlinable internal func indexFor(row: Index, column: Index) -> Index {
         switch order {
         case .rowMajor:
@@ -88,15 +134,20 @@ public class Matrix<T>: ExpressibleByArrayLiteral {
         }
     }
 
+    /// Get an element at the given row and column.
     @inlinable public func getElement(row: Index, column: Index) -> Element {
         return storage[indexFor(row: row, column: column)]
     }
 
+    /// Set an element at the given row and column.
     @inlinable public func setElement(_ element: Element, row: Index, column: Index) {
         storage[indexFor(row: row, column: column)] = element
     }
 
-    subscript(row: Index, column: Index) -> Element {
+    /// Access elements in the matrix with a *row*, *column* subscript.
+    ///
+    /// The order will always be (*row*, *column*).
+    public subscript(row: Index, column: Index) -> Element {
         get {
             return getElement(row: row, column: column)
         }
@@ -110,6 +161,12 @@ public class Matrix<T>: ExpressibleByArrayLiteral {
 // MARK: Standard Protocols
 
 extension Matrix: Sequence {
+    /// Create an iterator over each Element in a matrix.
+    ///
+    /// The order of elements from the iterator will depend on how the matrix is
+    /// stored internally, eithr row-major or column-major.
+    ///
+    /// - Returns: An iterator over each element individually.
     public func makeIterator() -> IndexingIterator<ContiguousArray<Element>> {
         return storage.makeIterator()
     }
@@ -118,17 +175,25 @@ extension Matrix: Sequence {
 // MARK: Type Restricted Protocols
 
 extension Matrix where Element: AdditiveArithmetic {
+    /// Initializer for numerical types, where the default fill value is 0.
     public convenience init(rows: Index, columns: Index, order: Order = .rowMajor) {
         self.init(rows: rows, columns: columns, initialValue: Element.zero, order: order)
     }
 }
 
 extension Matrix: Equatable where Element: Hashable {
+    /// Retursn whether or not two matrices are equal based on each element.
     public static func == (lhs: Matrix<Element>, rhs: Matrix<Element>) -> Bool {
         // NOTE: possible vectorization point
         return lhs.count == rhs.count && zip(lhs, rhs).allSatisfy { $0 == $1 }
     }
 
+    /// Allow matrices to be hashed on their values.
+    ///
+    /// The hash value *does* depend on the `order` of the matrix.
+    ///
+    /// - Parameter hasher: The Hasher instance used to calculate the hash.
+    // The parameter docstring is to get swift-format to shut up.
     public func hash(into hasher: inout Hasher) {
         self.forEach { hasher.combine($0) }
     }
--- Sources/Keanu/MatrixView.swift
+++ Sources/Keanu/MatrixView.swift
@@ -1,8 +1,18 @@
+/// Shared functionality between ColumnView and RowView.
 public class MatrixView<T> {
+    /// The type of element being stored within the matrix.
     public typealias Element = T
+
+    /// The type of the values used to access elements.
     public typealias Index = Int
+
+    /// The matrix that this view belongs to.
     public private(set) var matrix: Matrix<T>
+
+    /// Whether this view is a row or column view.
     private let viewType: Order
+
+    /// The index of the row or column in the matrix that this view covers.
     private let orderIndex: Index
 
     fileprivate init(_ matrix: Matrix<Element>, order: Order, index: Index) {
@@ -12,12 +22,14 @@ public class MatrixView<T> {
     }
 }
 
+/// A view over a single row in the matrix.
 public class RowView<T>: MatrixView<T> {
     internal init(_ matrix: Matrix<Element>, index: Index) {
         super.init(matrix, order: .rowMajor, index: index)
     }
 }
 
+/// A view over a single column in the matrix.
 public class ColumnView<T>: MatrixView<T> {
     internal init(_ matrix: Matrix<Element>, index: Index) {
         super.init(matrix, order: .columnMajor, index: index)
@@ -25,10 +37,13 @@ public class ColumnView<T>: MatrixView<T> {
 }
 
 extension MatrixView: MutableCollection {
+    /// The beginning of the indices for this collection.
     public var startIndex: Index {
         return Index(0)
     }
 
+    /// The index for the position just past the last element in this
+    /// collection.
     public var endIndex: Index {
         switch viewType {
         case .rowMajor:
@@ -38,10 +53,13 @@ extension MatrixView: MutableCollection {
         }
     }
 
+    /// The index immediately after the given index.
+    // Required to conform to Collection
     public func index(after i: Index) -> Index {
         return i + 1
     }
 
+    /// Access a specific element in this view.
     public subscript(index: Index) -> Element {
         get {
             switch viewType {
@@ -63,12 +81,18 @@ extension MatrixView: MutableCollection {
 }
 
 extension MatrixView: Equatable where Element: Hashable {
+    /// Return a boolean if this view and another view have the same values and
+    /// both are the same kind of view (row vs. column).
     public static func == (lhs: MatrixView<T>, rhs: MatrixView<T>) -> Bool {
         // NOTE: possible vectorization point
-        return lhs.count == rhs.count && zip(lhs, rhs).allSatisfy { $0 == $1 }
+        return lhs.count == rhs.count && lhs.viewType == rhs.viewType
+            && zip(lhs, rhs).allSatisfy { $0 == $1 }
     }
 
+    /// Generate a hash value for this view.
     public func hash(into hasher: inout Hasher) {
+        hasher.combine(viewType)
+        hasher.combine(orderIndex)
         self.forEach { hasher.combine($0) }
     }
 }
