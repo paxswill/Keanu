ca2ad0ccdb3c3e64c3f30b815b895711bc941e42
Will Ross <paxswill@paxswill.com>
2020-04-18 17:01:23 -0400

    Enable check_phase.sh to work from a shell
    SRCROOT would normally be provided by Xcode, so this gives it a 
    reasonable default.

--- BuildTools/check_phase.sh
+++ BuildTools/check_phase.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+# Provide a default value of SRCROOT if it hasn't been given
+if [ "x" = "x${SRCROOT}" ]; then
+	_OLD_DIR="$PWD"
+	cd `dirname "${PWD}"/"$0"/`/..
+	SRCROOT="$PWD"
+	cd "$_OLD_DIR"
+	unset _OLD_DIR
+fi
 swift run -c release --package-path "$SRCROOT"/BuildTools --skip-update swift-format lint -r  "$SRCROOT"/Sources "$SRCROOT"/Tests "$SRCROOT"/Package.swift
