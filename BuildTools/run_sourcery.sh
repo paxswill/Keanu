4bb61ac052e5b34fe896c6a0ff5ec224204cac30
Will Ross <paxswill@paxswill.com>
2020-04-21 17:10:54 -0400

    Add script for running Sourcery

---
+++ BuildTools/run_sourcery.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+# Provide a default value of SRCROOT if it hasn't been given
+if [ "x" = "x${SRCROOT}" ]; then
+    _OLD_DIR="$PWD"
+    if [ -f "$0" ]; then
+        cd "`dirname "$0"`/.."
+    else
+        cd `dirname "${PWD}"/"$0"/`/..
+    fi
+    export SRCROOT="$PWD"
+    cd "$_OLD_DIR"
+    unset _OLD_DIR
+fi
+"${SRCROOT}/BuildTools/run-build-tool.sh" \
+    sourcery \
+	--config "${SRCROOT}/.sourcery.yml"
